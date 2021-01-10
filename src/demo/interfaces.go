1f4b157d8e1d954e6de2c5aa920fbe127eb763e5
1-sw <cppa619@gmail.com>
2021-01-10 06:21:28 -0500

    Add byte interface and structs to interface demo

--- src/demo/interfaces.go
+++ src/demo/interfaces.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+
 type BaseValue interface {
     SetTrueValue() bool
     SetFalseValue() bool
@@ -32,9 +33,53 @@ func CheckNewBit(bit BaseValue) {
     fmt.Println(bit.GetValue())
 }
 
+
+type ByteMethods interface {
+
+    GetAllBits() []Bit
+    GetBitByIndex(index int) Bit
+
+    PushBit()
+    PopBit()
+
+}
+
+type Byte struct {
+    Bits []Bit
+}
+
+func (ByteV Byte) GetAllBits() []Bit {
+    return ByteV.Bits
+}
+
+func (ByteV Byte) GetBitByIndex(index int) Bit {
+    return Bit{
+        _Value_ : bool(ByteV.Bits[index].GetValue()),
+    }
+}
+
+func (ByteV Byte) PushBit() {
+    fmt.Println("In process")
+}
+
+func (byteV Byte) PopBit() {
+    fmt.Println("In process")
+}
+
+func CheckNewByte(ByteV ByteMethods) {
+    fmt.Println("Maded new Byte")
+}
+
 func main() {
+
     var bit Bit = Bit {
         _Value_ : true,
     }
+
+    var ByteV Byte = Byte {
+        Bits : []Bit{bit},
+    }
+
     CheckNewBit(bit)
+    CheckNewByte(ByteV)
 }
